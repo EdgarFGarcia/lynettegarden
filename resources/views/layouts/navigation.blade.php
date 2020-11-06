3179372edf0fbd8f36ca30bf27522eed6a147826
EdgarFGarcia <edgarfrancisgarcia7@gmail.com>
2020-11-07 03:39:23 +0800

    added admin dashboard

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,98 @@
+<div class="sidebar" data-color="white" data-active-color="danger">
+      <div class="logo">
+        <a href="{{ url('/home') }}" class="simple-text logo-mini">
+          <div class="logo-image-small">
+            <img src="{{asset('assets/img/logo-small.png')}}">
+          </div>
+          <!-- <p>CT</p> -->
+        </a>
+        <a href="{{ url('/home') }}" class="simple-text logo-normal">
+          Lynette Garden
+          <!-- <div class="logo-image-big">
+            <img src="{{asset('assets/img/logo-big.png')}}">
+          </div> -->
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="active ">
+            <a href="{{ url('/home') }}">
+              <i class="nc-icon nc-circle-10"></i>
+              <p>Manage User</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="nc-icon nc-book-bookmark"></i>
+              <p>Manage Reservation</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="nc-icon nc-alert-circle-i"></i>
+              <p>Cancel Reservation</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="nc-icon nc-box"></i>
+              <p>Confirm Cancellation</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="nc-icon nc-bold"></i>
+              <p>Billing</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="nc-icon nc-bookmark-2"></i>
+              <p>Payment</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="nc-icon nc-money-coins"></i>
+              <p>Generate Report</p>
+            </a>
+          </li>
+          <!-- <li>
+            <a href="./map.html">
+              <i class="nc-icon nc-pin-3"></i>
+              <p>Maps</p>
+            </a>
+          </li>
+          <li>
+            <a href="./notifications.html">
+              <i class="nc-icon nc-bell-55"></i>
+              <p>Notifications</p>
+            </a>
+          </li>
+          <li>
+            <a href="./user.html">
+              <i class="nc-icon nc-single-02"></i>
+              <p>User Profile</p>
+            </a>
+          </li>
+          <li>
+            <a href="./tables.html">
+              <i class="nc-icon nc-tile-56"></i>
+              <p>Table List</p>
+            </a>
+          </li>
+          <li>
+            <a href="./typography.html">
+              <i class="nc-icon nc-caps-small"></i>
+              <p>Typography</p>
+            </a>
+          </li>
+          <li class="active-pro">
+            <a href="./upgrade.html">
+              <i class="nc-icon nc-spaceship"></i>
+              <p>Upgrade to PRO</p>
+            </a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/logout', function(){
+    Auth::logout();
+    return Redirect::to('login');
+});
